662749ea4ebd333230f040b077e7cf0649de816a
Arsen Hryhoriak <136361705+Lifuss@users.noreply.github.com>
2023-10-12 14:41:56 +0300

    routing

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -4,16 +4,22 @@ import Header from 'components/Header/Header';
 import Loader from 'components/Loader/Loader';
 import Navigation from 'components/Navigation/Navigation';
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 const DashboardPage = () => {
   return (
-    <div>
-      <Header />
-      <Navigation />
-      {/* <Currency /> дивлячись який адаптив */}
-      <Balance />
-      <Loader />
-    </div>
+    <>
+      <div>
+        <Header />
+        <Navigation />
+        {/* <Currency /> дивлячись який адаптив */}
+        <Balance />
+        <Loader />
+      </div>
+
+      <hr />
+      <Outlet />
+    </>
   );
 };
 
