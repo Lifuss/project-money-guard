65a2495c5403b55166f0e3fe679f1faa491fee7d
Vitalii Dimov <113688545+Carriedbydog@users.noreply.github.com>
2023-10-12 22:43:41 +0300

    styles for Transaction Item
    wrote a styles for item using styled inside TransactionsItem.styled.js

--- src/components/TransactionsItem/TransactionsItem.jsx
+++ src/components/TransactionsItem/TransactionsItem.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteTransactionThunk } from 'redux/transactions/operations';
-import { StyledDeleteBtn } from './TransactionsItem.styled';
+import {
+  StyledBtnBox,
+  StyledDeleteBtn,
+  StyledEditBtn,
+  StyledParWrapper,
+  StyledParagraph,
+  StyledTransaction,
+} from './TransactionsItem.styled';
 import { selectCategories } from 'redux/transactions/selectors';
+import sprite from '../../images/sprite.svg';
 
 const TransactionsItem = ({ transaction }) => {
   const categories = useSelector(selectCategories);
@@ -12,21 +20,46 @@ const TransactionsItem = ({ transaction }) => {
     dispatch(deleteTransactionThunk(id));
   };
   return (
-    <li>
-      <p>
-        Date<span>{transaction.transactionDate}</span>
-      </p>
-      <p>Type{transaction.type}</p>
-      <p>Category{categories ? category.name : 'N/A'}</p>
-      <p>Comment{transaction.comment}</p>
-      <p>Sum{transaction.amount}</p>
-      <div>
+    <StyledTransaction
+      style={{
+        borderLeft:
+          transaction.type === 'INCOME'
+            ? '5px solid #ff868d'
+            : '5px solid #FFB627',
+      }}
+    >
+      <StyledParWrapper>
+        <StyledParagraph>Date</StyledParagraph>
+        <span>{transaction.transactionDate}</span>
+      </StyledParWrapper>
+      <StyledParWrapper>
+        <StyledParagraph>Type</StyledParagraph>
+        <span>{transaction.type}</span>
+      </StyledParWrapper>
+      <StyledParWrapper>
+        <StyledParagraph>Category</StyledParagraph>
+        <span>{categories ? category.name : '-'}</span>
+      </StyledParWrapper>
+      <StyledParWrapper>
+        <StyledParagraph>Comment</StyledParagraph>
+        <span>{transaction.comment}</span>
+      </StyledParWrapper>
+      <StyledParWrapper>
+        <StyledParagraph>Sum</StyledParagraph>
+        <span>{transaction.amount}</span>
+      </StyledParWrapper>
+      <StyledBtnBox>
         <StyledDeleteBtn onClick={() => handleBtnDelete(transaction.id)}>
           Delete
         </StyledDeleteBtn>
-        <button>Edit</button>
-      </div>
-    </li>
+        <StyledEditBtn>
+          <svg width="14" height="14">
+            <use href={`${sprite}#edit`} />
+          </svg>
+          Edit
+        </StyledEditBtn>
+      </StyledBtnBox>
+    </StyledTransaction>
   );
 };
 
--- src/components/TransactionsItem/TransactionsItem.styled.js
+++ src/components/TransactionsItem/TransactionsItem.styled.js
@@ -1,5 +1,65 @@
 import styled from 'styled-components';
 
+export const StyledTransaction = styled.li`
+  margin: 0 auto;
+  width: 280px;
+  border-radius: 10px;
+  background: rgba(255, 255, 255, 0.1);
+  border-left: 5px solid #ff868d;
+`;
+
+export const StyledParWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 12px 20px;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.41);
+`;
+export const StyledParagraph = styled.p`
+  text-align: right;
+  font-family: Poppins;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  color: #fbfbfb;
+`;
+
+export const StyledBtnBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 12px 20px;
+`;
+
 export const StyledDeleteBtn = styled.button`
+  text-align: center;
+  font-family: Poppins;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  padding: 4px 12px;
+
   background: ${({ theme }) => theme.backgrounds.btnGradient};
+  border-radius: 18px;
+  box-shadow: 1px 9px 15px 0px rgba(0, 0, 0, 0.2);
+`;
+
+export const StyledEditBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+  padding: 4px 12px;
+
+  background-color: transparent;
+
+  color: var(--white-60, rgba(255, 255, 255, 0.6));
+  text-align: center;
+  font-family: Poppins;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 0.32px;
 `;
