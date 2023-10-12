abc6669a3b99417812377e1f078596ad2fa8c1f9
Vitalii Dimov <113688545+Carriedbydog@users.noreply.github.com>
2023-10-12 20:50:20 +0300

    category
    made a category  and added hardcode samples in category array

--- src/components/TransactionsItem/TransactionsItem.jsx
+++ src/components/TransactionsItem/TransactionsItem.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { deleteTransactionThunk } from 'redux/transactions/operations';
 import { StyledDeleteBtn } from './TransactionsItem.styled';
+import { selectCategories } from 'redux/transactions/selectors';
 
 const TransactionsItem = ({ transaction }) => {
+  const categories = useSelector(selectCategories);
+  const category = categories.find(cat => cat.id === transaction.categoryId);
   const dispatch = useDispatch();
   const handleBtnDelete = id => {
     dispatch(deleteTransactionThunk(id));
@@ -14,7 +17,7 @@ const TransactionsItem = ({ transaction }) => {
         Date<span>{transaction.transactionDate}</span>
       </p>
       <p>Type{transaction.type}</p>
-      <p>Category{transaction.categoryId}</p>
+      <p>Category{categories ? category.name : 'N/A'}</p>
       <p>Comment{transaction.comment}</p>
       <p>Sum{transaction.amount}</p>
       <div>
