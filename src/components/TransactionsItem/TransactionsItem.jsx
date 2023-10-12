252f01b5df57a33a022884902c4c9bc17b407e98
Vitalii Dimov <113688545+Carriedbydog@users.noreply.github.com>
2023-10-12 18:19:03 +0300

    transaction List/Item
    Markup transactionList and transaction Item components.

--- src/components/TransactionsItem/TransactionsItem.jsx
+++ src/components/TransactionsItem/TransactionsItem.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteTransactionThunk } from 'redux/transactions/operations';
 
-const TransactionsItem = () => {
-  return <div>TransactionsItem</div>;
+const TransactionsItem = ({ transaction }) => {
+  const dispatch = useDispatch();
+  const handleBtnDelete = id => {
+    dispatch(deleteTransactionThunk(id));
+  };
+  return (
+    <li>
+      <p>
+        Date<span>{transaction.transactionDate}</span>
+      </p>
+      <p>Type{transaction.type}</p>
+      <p>Category{transaction.categoryId}</p>
+      <p>Comment{transaction.comment}</p>
+      <p>Sum{transaction.amount}</p>
+      <div>
+        <button onClick={() => handleBtnDelete(transaction.id)}>Delete</button>
+        <button>Edit</button>
+      </div>
+    </li>
+  );
 };
 
 export default TransactionsItem;
--- src/components/TransactionsList/TransactionsList.jsx
+++ src/components/TransactionsList/TransactionsList.jsx
@@ -1,7 +1,31 @@
+import TransactionsItem from 'components/TransactionsItem/TransactionsItem';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import {
+  selectError,
+  selectLoading,
+  selectTransactions,
+} from 'redux/transactions/selectors';
 
 const TransactionsList = () => {
-  return <div>TransactionsList</div>;
+  const transactions = useSelector(selectTransactions);
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+  return (
+    <div>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Something went wrong... 😢</h1>}
+      {transactions ? (
+        <ul>
+          {transactions?.map(transaction => (
+            <TransactionsItem key={transaction.id} transaction={transaction} />
+          ))}
+        </ul>
+      ) : (
+        <h3>Oh you dont have transactions yet, lets create it!</h3>
+      )}
+    </div>
+  );
 };
 
 export default TransactionsList;
