faeb27bad062060d9866ea8332e56dfe2521db3a
Arsen Hryhoriak <136361705+Lifuss@users.noreply.github.com>
2023-10-12 20:15:43 +0300

    PageNotFound

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return (
+    <StyledBox>
+      <StyledTitle>Page is not found</StyledTitle>
+      <Link to={'/'}>
+        Go <StyledLink>home</StyledLink>
+      </Link>
+    </StyledBox>
+  );
 };
+
+const StyledTitle = styled.h1`
+  font-size: 40px;
+`;
+
+export const StyledBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 70vh;
+`;
+
+export const StyledLink = styled.span`
+  text-decoration: underline;
+  color: ${({ theme }) => theme.colors.incomeColor};
+`;
 
 export default PageNotFound;
