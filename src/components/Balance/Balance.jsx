54b7f3c6be300c64bce09d7f3d986d3a1ea91938
Arsen Hryhoriak <136361705+Lifuss@users.noreply.github.com>
2023-10-12 21:57:34 +0300

    sprite fix

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -5,15 +5,13 @@ import {
   StyledBalanceTitle,
 } from './Balance.styled';
 
+
 const Balance = () => {
   const mockBalance = 24000;
   return (
     <StyledBalanceDiv>
       <StyledBalanceSupTitle>Your Balance</StyledBalanceSupTitle>
       <StyledBalanceTitle>&#8372;{mockBalance.toFixed(2)}</StyledBalanceTitle>
-      <svg width="20" height="20">
-        <use href="../../images/sprite.svg#edit"></use>
-      </svg>
     </StyledBalanceDiv>
   );
 };
