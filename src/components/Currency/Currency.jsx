01286777f63013cca4dc360e5ef0853f54bed8a7
Pavel-K-ua <113534910+Pavel-K-ua@users.noreply.github.com>
2023-10-13 15:08:24 +0300

    add logic
    get data from API
    set in local storage
    in 5 minutes(for testing) data takes from local storage, then again once from API

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -1,47 +1,49 @@
-import React from 'react';
-
-const data = [
-  {
-    currencyCodeA: 840,
-    currencyCodeB: 980,
-    date: 1697144473,
-    rateBuy: 36.44,
-    rateCross: 0,
-    rateSell: 37.4406,
-  },
-  {
-    currencyCodeA: 978,
-    currencyCodeB: 980,
-    date: 1697144473,
-    rateBuy: 38.64,
-    rateCross: 0,
-    rateSell: 39.7504,
-  },
-  {
-    currencyCodeA: 978,
-    currencyCodeB: 840,
-    date: 1697144473,
-    rateBuy: 1.051,
-    rateCross: 0,
-    rateSell: 1.065,
-  },
-  {
-    currencyCodeA: 826,
-    currencyCodeB: 980,
-    date: 1697144811,
-    rateBuy: 0,
-    rateCross: 46.164,
-    rateSell: 0,
-  },
-];
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Currency = () => {
-  const rateUSD = data.find(obj => obj.currencyCodeA === 840);
-  const rateEUR = data.find(obj => obj.currencyCodeA === 978);
-  const rateBuyUSD = rateUSD.rateBuy.toFixed(2);
-  const rateSellUSD = rateUSD.rateSell.toFixed(2);
-  const rateBuyEUR = rateEUR.rateBuy.toFixed(2);
-  const rateSellEUR = rateEUR.rateSell.toFixed(2);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchDataAndStore = async () => {
+      try {
+        const response = await axios.get(
+          'https://api.monobank.ua/bank/currency'
+        );
+        const newData = response.data;
+        const fetchTime = new Date().getTime();
+        localStorage.setItem(
+          'MONO',
+          JSON.stringify({ data: newData, fetchTime })
+        );
+        setData(newData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    const storedData = localStorage.getItem('MONO');
+    if (storedData) {
+      const { data, fetchTime } = JSON.parse(storedData);
+      const currentTime = new Date().getTime();
+      console.log(currentTime);
+      console.log(fetchTime);
+      if (currentTime - fetchTime < 300000) {
+        setData(data);
+      } else {
+        fetchDataAndStore();
+      }
+    } else {
+      fetchDataAndStore();
+    }
+  }, []);
+
+  const rateUSD = data?.find(obj => obj.currencyCodeA === 840);
+  const rateEUR = data?.find(obj => obj.currencyCodeA === 978);
+  const rateBuyUSD = rateUSD?.rateBuy.toFixed(2);
+  const rateSellUSD = rateUSD?.rateSell.toFixed(2);
+  const rateBuyEUR = rateEUR?.rateBuy.toFixed(2);
+  const rateSellEUR = rateEUR?.rateSell.toFixed(2);
 
   return (
     <div>
