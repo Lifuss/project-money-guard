a250cb2020b44c0f6fa56a0ebc0f63c28b00538b
Yanaredko <136071376+Yanaredko@users.noreply.github.com>
2023-10-13 13:20:30 +0200

    login without styling

---
+++ src/components/LoginForm/LoginForm.styled.js
@@ -0,0 +1,69 @@
+const styles = {
+    containerStyle: {
+    width: '533px',
+    height: '570px',
+    margin: 'auto',
+    background: 'rgba(255, 255, 255, 0.10)',
+    boxShadow: '0px 4px 60px rgba(0, 0, 0, 0.25)',
+    borderRadius: 8,
+    backdropFilter: 'blur(100px)',
+    borderWidth: '10px',
+    borderStyle: 'solid',
+    borderColor: 'red'
+  },
+    
+  titleStyle: {
+    fontSize: '24px',
+    color: '#FBFBFB',
+    // fontFamily: 'Poppins',
+    // fontWeight: '400',
+    // marginBottom: '20px',
+  },
+  emailLabelStyle: {
+    color: 'rgba(255, 255, 255, 0.60)',
+    fontSize: '18px',
+    // fontFamily: 'Poppins',
+    // fontWeight: '400',
+  },
+  emailInputStyle: {
+    width: '100%',
+    padding: '8px',
+    marginBottom: '10px',
+  },
+  passwordLabelStyle: {
+    color: 'rgba(255, 255, 255, 0.60)',
+    fontSize: '18px',
+    // fontFamily: 'Poppins',
+    // fontWeight: '400',
+  },
+  passwordInputStyle: {
+    width: '100%',
+    padding: '8px',
+    marginBottom: '10px',
+  },
+//   inputContainerStyle: {
+//     marginBottom: '20px',
+//   },
+  registrationLinkStyle: {
+    color: '#623F8B',
+    fontSize: '18px',
+    // fontFamily: 'Poppins',
+    // fontWeight: '400',
+    textDecoration: 'none',
+  },
+  submitButtonStyle: {
+    width: '100%',
+    // background: '#FCFCFC',
+    padding: '10px',
+    fontSize: '18px',
+    // fontFamily: 'Poppins',
+    // fontWeight: '400',
+    textTransform: 'uppercase',
+    letterSpacing: '1.80px',
+    borderRadius: '20px',
+    border: '1px #4A56E2 solid',
+    cursor: 'pointer',
+  },
+};
+
+export default styles;
