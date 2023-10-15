dff9339259433b9cb215e22cfd8adfd0b39f2037
DianaKryzhanivska <di_parkhomenko@ukr.net>
2023-10-15 16:21:11 +0300

    Custom switcher for transaction type done

--- src/components/AddTransactionForm/AddTransactionForm.styled.js
+++ src/components/AddTransactionForm/AddTransactionForm.styled.js
@@ -40,6 +40,58 @@ export const AddFormTitle = styled.h2`
   margin-bottom: 34px;
 `;
 
+export const StyledRadioBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+`;
+
+export const StyledRadioInput = styled.input`
+  display: none;
+`;
+
+export const StyledTextSpan = styled.span`
+  color: rgba(255, 255, 255, 0.6);
+  font-size: 16px;
+  font-weight: 600;
+`;
+
+export const SwitcherSquare = styled.div`
+  width: 80px;
+  height: 40px;
+  border-radius: 30px;
+  background: #fff;
+  position: relative;
+`;
+
+export const SwitcherRoundPlus = styled.div`
+  width: 44px;
+  height: 44px;
+  background: #ffb627;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: -2px;
+  left: -2px;
+`;
+
+export const SwitcherRoundMinus = styled.div`
+  width: 44px;
+  height: 44px;
+  background: #ff868d;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: -2px;
+  left: 38px;
+`;
+
 export const StyledDatePicker = styled(DatePicker)`
   width: 280px;
   height: 75px;
